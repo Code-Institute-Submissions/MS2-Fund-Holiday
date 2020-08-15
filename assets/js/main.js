35eb800864b474e91914fb678079cafddcbde018
gerd113 <gerd112@hotmail.com>
2020-08-15 16:20:49 +0000

    Added variables o get the user's date and time.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,5 +8,17 @@ const type = "&type=national"
 //suggestion from anna_ci chat thread of Nov 5th 2019. different site used. Api Key
 let _0x90dd = ["\x61\x70\x69\x5F\x6B\x65\x79\x3D\x66\x33\x37\x36\x65\x61\x61\x61\x35\x36\x62\x31\x34\x33\x32\x35\x34\x39\x32\x36\x65\x31\x61\x65\x61\x63\x66\x62\x64\x65\x63\x31\x65\x32\x34\x37\x30\x66\x35\x30"];
 const apk = _0x90dd;
+//
 
+
+// Code adapted from https://www.w3schools.com/js/js_date_methods.asp
+let userSystemDate = new Date();
+let userSystemYear = userSystemDate.getFullYear();
+let userSystemMonth = userSystemDate.getMonth();
+//
+
+
+console.log(userSystemDate);
+console.log(userSystemYear);
+console.log(userSystemMonth);  
 
