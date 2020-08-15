09bfc23efbed18324d097cf2ec4c93fd69a141c2
gerd113 <gerd112@hotmail.com>
2020-08-15 15:27:16 +0000

    Added constant variables for api calls.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
+const baseURL = "https://calendarific.com/api/v2/holidays?";
+const countryFormat = "&country=";
+const yearFormat = "&year=";
+const monthFormat = "&month=";
+const type = "&type=national"
+
+//https://www.javascriptobfuscator.com/Javascript-Obfuscator.aspx
+//suggestion from anna_ci chat thread of Nov 5th 2019. different site used. Api Key
+let _0x90dd = ["\x61\x70\x69\x5F\x6B\x65\x79\x3D\x66\x33\x37\x36\x65\x61\x61\x61\x35\x36\x62\x31\x34\x33\x32\x35\x34\x39\x32\x36\x65\x31\x61\x65\x61\x63\x66\x62\x64\x65\x63\x31\x65\x32\x34\x37\x30\x66\x35\x30"];
+const apk = _0x90dd;
+
 
