8238335982f38f1ef7506ac8e10e8ca2b77ddec6
gerd113 <gerd112@hotmail.com>
2020-08-15 18:37:21 +0000

    Added getCountryHolidays function to gt the ISO country code fro the users selection.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,15 @@ const baseURL = "https://calendarific.com/api/v2/holidays?";
 const countryFormat = "&country=";
 const yearFormat = "&year=";
 const monthFormat = "&month=";
-const type = "&type=national"
+const type = "&type=national";
 
 //https://www.javascriptobfuscator.com/Javascript-Obfuscator.aspx
 //suggestion from anna_ci chat thread of Nov 5th 2019. different site used. Api Key
-let _0x90dd = ["\x61\x70\x69\x5F\x6B\x65\x79\x3D\x66\x33\x37\x36\x65\x61\x61\x61\x35\x36\x62\x31\x34\x33\x32\x35\x34\x39\x32\x36\x65\x31\x61\x65\x61\x63\x66\x62\x64\x65\x63\x31\x65\x32\x34\x37\x30\x66\x35\x30"];
+let _0x90dd = [
+  "\x61\x70\x69\x5F\x6B\x65\x79\x3D\x66\x33\x37\x36\x65\x61\x61\x61\x35\x36\x62\x31\x34\x33\x32\x35\x34\x39\x32\x36\x65\x31\x61\x65\x61\x63\x66\x62\x64\x65\x63\x31\x65\x32\x34\x37\x30\x66\x35\x30",
+];
 const apk = _0x90dd;
 //
-
 
 // Code adapted from https://www.w3schools.com/js/js_date_methods.asp
 let userSystemDate = new Date();
@@ -24,10 +25,13 @@ let nextMonth;
 
 // if current month is November the next month is December. If the current Month is December the next month is January.
 if (currentMonth < 12) {
-    nextMonth = currentMonth + 1;
+  nextMonth = currentMonth + 1;
 } else {
-    nextMonth = currentMonth - 11;
+  nextMonth = currentMonth - 11;
 }
- //
+//
 
-
+function getCountryHolidays() {
+  let iscde = document.getElementById("countryForm").value;
+  let isoCode = iscde.slice(-2);
+}
